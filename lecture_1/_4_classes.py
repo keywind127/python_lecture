11b4407655ba16be3834bd7f77e6a846329e109a
Rivers <river910429@gmail.com>
2024-11-21 20:46:17 +0800

    feature:q3

--- lecture_1/_4_classes.py
+++ lecture_1/_4_classes.py
@@ -72,6 +72,9 @@ class Student(User):
     # implement your code here
     # :)
     # :)
+    @property
+    def student_info(self):
+        return self.name,self.sid
     
 student = Student("Kevin", 127)
 
